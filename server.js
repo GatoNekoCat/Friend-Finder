a99e80e3542447ef8c5641be96a1d5ffad9aff6f
Raiicon <orankai@gmail.com>
2018-12-17 17:52:04 -0500

    index brough over from example

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path'); 
+
+// express
+var app = express();
+var PORT = process.env.PORT || 8080;
