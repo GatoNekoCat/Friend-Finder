f9ef2ed2a3e58b6d9a25acc0619ae57e45ff2a1b
Raiicon <orankai@gmail.com>
2018-12-18 23:06:22 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -7,18 +7,7 @@ const path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-//Questions
-const qAndA = ["Your mind is always buzzing with unexplored ideas and plans", 
-              "Generally speaking, you rely more on your experience than your imagination", 
-              "You find it easy to stay relaxed and focused even when there is some pressure.", 
-              "You rarely do something just out of sheer curiosity", "People can rarely rarely upset you.", 
-              "It is often difficult for you to relate to other people's feelings.", 
-              "In a discussion, truth should be more important than people's sensitivities.", 
-              "You rarely get carried away by fantasies and ideas", 
-              "You think that everyone's views should be respected regardless of whether they are supported by facts or not", 
-              "You feel more energetic after spending time with a group of people"
-];
-/////
+
 
 
 // Basic route that sends the user to the index.html
